d2ff6248b817491d9e46212488d8e1d96ada2db1
leizhuang <hellolay5035@163.com>
2021-12-12 10:38:11 +0800

    修复了一个异常

--- src/main/java/com/leizhuang/controller/ArticleController.java
+++ src/main/java/com/leizhuang/controller/ArticleController.java
@@ -24,7 +24,7 @@ public class ArticleController {
      */
     @PostMapping
     public Result listArticle(@RequestBody PageParams pageParams) {
-int i=10/0;
+
         return articleService.listArticle(pageParams);
     }
 
