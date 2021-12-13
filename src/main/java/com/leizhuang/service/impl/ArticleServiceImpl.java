b17dc8b15d9c5da93b97e579d0345ca361305137
leizhuang <hellolay5035@163.com>
2021-12-13 10:28:42 +0800

    解决了id精度丢失

--- src/main/java/com/leizhuang/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/leizhuang/service/impl/ArticleServiceImpl.java
@@ -93,7 +93,15 @@ public class ArticleServiceImpl implements ArticleService {
 
         return articleVoList;
     }
+/*//    重载
+    private List<ArticleVo> copyList(List<Article> records, boolean isTag, boolean isAuthor,boolean isBody,boolean isCategory) {
+        List<ArticleVo> articleVoList = new ArrayList<>();
+        for (Article record : records) {
+            articleVoList.add(copy(record, isTag, isAuthor,isBody,isCategory));
+        }
 
+        return articleVoList;
+    }*/
     @Override
     public Result findArticleById(Long articleId) {
         /**
--- src/main/java/com/leizhuang/vo/CategoryVo.java
+++ src/main/java/com/leizhuang/vo/CategoryVo.java
@@ -1,9 +1,12 @@
 package com.leizhuang.vo;
 
+import lombok.Data;
+
 /**
  * @author LeiZhuang
  * @date 2021/12/13 9:26
  */
+@Data
 public class CategoryVo {
     private Long id;
     private String avatar;
